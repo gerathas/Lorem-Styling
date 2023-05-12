3f77644dbd7b96b23d499332303bf6c92d447553
gerathas <50587095+gerathas@users.noreply.github.com>
2023-05-12 10:03:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lorem-Styling
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-3pzhpd)
---
+++ src/App.tsx
@@ -0,0 +1,123 @@
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+import Input from './Input.tsx';
+import { useState } from 'react';
+
+function App() {
+  var paragraph =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam odio ut sapien mollis hendrerit. Fusce in lectus ex. In hac habitasse platea dictumst. Duis aliquam sed arcu at sagittis. Pellentesque suscipit elit a elit dignissim, ornare mattis libero convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque in dictum augue, nec consectetur enim. Pellentesque quis quam ultricies, volutpat nulla sit amet, egestas est. Praesent accumsan ante non dui sollicitudin convallis. Etiam facilisis finibus lectus, euismod gravida ipsum ultricies at.';
+
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [input3, setInput3] = useState('');
+
+  const handleInput1 = (event) => {
+    setInput1(event.target.value);
+  };
+  const handleInput2 = (event) => {
+    setInput2(event.target.value);
+  };
+  const handleInput3 = (event) => {
+    setInput3(event.target.value);
+  };
+
+  // Helper function to underline text from the first input
+  function getUnderlinedText(text, highlight) {
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+      <span>
+        {' '}
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { textDecorationLine: 'underline' }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}{' '}
+      </span>
+    );
+  }
+
+  // Helper function to bold text from the second input
+  function getBoldedText(text, highlight) {
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+      <span>
+        {' '}
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { fontWeight: 'bold' }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}{' '}
+      </span>
+    );
+  }
+
+  // Helper function to change text to red from the third input
+  function getRedText(text, highlight) {
+    // Split on highlight term and include term into parts, ignore case
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+      <span>
+        {' '}
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { color: 'red' }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}{' '}
+      </span>
+    );
+  }
+
+  return (
+    <>
+      {input1 ? (
+        getUnderlinedText(paragraph, input1)
+      ) : input2 ? (
+        getBoldedText(paragraph, input2)
+      ) : input3 ? (
+        getRedText(paragraph, input3)
+      ) : (
+        <span> {paragraph} </span>
+      )}
+      <div></div>
+      <div className="card">
+        <input
+          onChange={handleInput1}
+          placeholder="Enter text to be underlined."
+        />
+      </div>
+      <div>
+        <input onChange={handleInput2} placeholder="Enter text to be bolded." />
+      </div>
+      <div className="card">
+        <input
+          onChange={handleInput3}
+          placeholder="Enter text to appear red."
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
