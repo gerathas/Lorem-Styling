067169d7b657cf2156a670b44819e0e7b00d94ef
gerathas <50587095+gerathas@users.noreply.github.com>
2023-05-12 21:38:54 -0400

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
-import Input from './Input.tsx';
 import { useState } from 'react';
 
 function App() {
